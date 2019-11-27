3663bef6bd926b5d232fcecd451c01d81b0ef94e
buiduchoang92 <hoang92duc@gmail.com>
2019-11-27 20:01:13 +0700

    UpLoadBaiTapMoi

---
+++ Module1/NhapMonLapTinhCanBan/ThucHanh/SuDungToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the Width");
+inputHeight = prompt("End the Height");
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let area = Width*Height;
+document.writeln("the area is: " + area);
